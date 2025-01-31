1c94d7df5f5908a694e504f3b104f46539d4a740
MgKyaw <mgkyaw20112@gmail.com>
2025-01-31 21:26:21 +0700

    feat: Print int and decimal

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -21,7 +21,14 @@
 
 // int[] data = new int[3];
 
-int first = 2;
-string second = "4";
-string result = first + second;
-Console.WriteLine(result);
+// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
+
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}");
+
