036a5b79bcc58b47bc91d8ff015871b2a2c34b57
MgKyaw <mgkyaw20112@gmail.com>
2025-01-31 21:37:14 +0700

    feat: Use int.Parse()

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -44,7 +44,12 @@
 // Console.WriteLine($"Decimal: {myDecimal}");
 // Console.WriteLine($"Float  : {myFloat}");
 
-int first = 5;
-int second = 7;
-string message = first.ToString() + second.ToString();
-Console.WriteLine(message);
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+
+string first = "5";
+string second = "7";
+int sum = int.Parse(first) + int.Parse(second);
+Console.WriteLine(sum);
