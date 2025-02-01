494eb2153a2055ad036fea53de15a5344c6191ca
MgKyaw <mgkyaw20112@gmail.com>
2025-02-01 08:47:27 +0700

    feat: Comment unit 3

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -65,15 +65,18 @@
 // int value2 = Convert.ToInt32(1.5m); // converting rounds up
 // Console.WriteLine(value2);
 
-string value = "bad";
-int result = 0;
-if (int.TryParse(value, out result))
-{
-   Console.WriteLine($"Measurement: {result}");
-}
-else
-{
-   Console.WriteLine("Unable to report the measurement.");
-}
-if (result > 0)
-    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+// Unit 3
+
+// string value = "bad";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//    Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//    Console.WriteLine("Unable to report the measurement.");
+// }
+// if (result > 0)
+//     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
