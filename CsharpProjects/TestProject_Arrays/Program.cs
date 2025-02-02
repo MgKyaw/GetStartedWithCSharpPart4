d11b34e468ff5158ea79506e0234bc167da0ef0c
MgKyaw <mgkyaw20112@gmail.com>
2025-02-02 21:41:04 +0700

    feat: Test Array.Clear method

--- CsharpProjects/TestProject_Arrays/Program.cs
+++ CsharpProjects/TestProject_Arrays/Program.cs
@@ -18,3 +18,16 @@
 // {
 //     Console.WriteLine($"-- pallet");
 // }
+
+// Unit 3
+
+string[] pallets = [ "B14", "A11", "B12", "A13" ];
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
