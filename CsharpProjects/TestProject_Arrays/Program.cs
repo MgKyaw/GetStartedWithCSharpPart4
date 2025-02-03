3823af828c374b3a868eec67b3d37e540608fb7e
MgKyaw <mgkyaw20112@gmail.com>
2025-02-03 21:54:48 +0700

    feat: Comment unit 3

--- CsharpProjects/TestProject_Arrays/Program.cs
+++ CsharpProjects/TestProject_Arrays/Program.cs
@@ -21,15 +21,15 @@
 
 // Unit 3
 
-string[] pallets = [ "B14", "A11", "B12", "A13" ];
-Console.WriteLine("");
+// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+// Console.WriteLine("");
 
-Console.WriteLine($"Before: {pallets[0]}");
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"After: {pallets[0]}");
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// Console.WriteLine($"Before: {pallets[0]}");
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"After: {pallets[0]}");
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
