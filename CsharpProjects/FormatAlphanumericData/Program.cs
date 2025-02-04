e8c70d21a10b2ab2f11dcc21193bd44fdb3fd307
MgKyaw <mgkyaw20112@gmail.com>
2025-02-04 22:23:33 +0700

    feat: Test String Interpolation

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -6,3 +6,8 @@ string second = "World";
 // // Composite Formatting
 // Console.WriteLine("{1} {0}!", first, second);
 // Console.WriteLine("{0} {0} {0}!", first, second);
+
+// String Interpolation
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
