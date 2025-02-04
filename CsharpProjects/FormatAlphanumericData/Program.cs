3916ca150c0e97de258e61bc78b1a47dd4f8961d
MgKyaw <mgkyaw20112@gmail.com>
2025-02-04 22:24:42 +0700

    feat: Add using composite formatting

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -4,6 +4,9 @@ string first = "Hello";
 string second = "World";
 
 // // Composite Formatting
+// string result = string.Format("{0} {1}!", first, second);
+// Console.WriteLine(result);
+
 // Console.WriteLine("{1} {0}!", first, second);
 // Console.WriteLine("{0} {0} {0}!", first, second);
 
