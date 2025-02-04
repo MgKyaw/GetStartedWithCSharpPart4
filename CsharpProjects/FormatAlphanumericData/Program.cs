6c814f5b8cb946ef4c802e1a4cb85e5dd05e0f99
MgKyaw <mgkyaw20112@gmail.com>
2025-02-04 22:19:24 +0700

    feat: Modify the formatting

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -2,5 +2,5 @@
 
 string first = "Hello";
 string second = "World";
-string result = string.Format("{0} {1}!", first, second);
-Console.WriteLine(result);
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
