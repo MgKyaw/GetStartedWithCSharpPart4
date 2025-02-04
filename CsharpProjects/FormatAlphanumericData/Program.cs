58f9659134e237ae5fe06f1ebfe4abbcf8fbef29
MgKyaw <mgkyaw20112@gmail.com>
2025-02-04 22:21:02 +0700

    feat: Comment composite formatting

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -3,6 +3,6 @@
 string first = "Hello";
 string second = "World";
 
-// Composite Formatting
-Console.WriteLine("{1} {0}!", first, second);
-Console.WriteLine("{0} {0} {0}!", first, second);
+// // Composite Formatting
+// Console.WriteLine("{1} {0}!", first, second);
+// Console.WriteLine("{0} {0} {0}!", first, second);
