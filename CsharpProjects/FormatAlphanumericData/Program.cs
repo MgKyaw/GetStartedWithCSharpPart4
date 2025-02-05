47dba22e197a40ad816fe66e3ba8f1604caf4d3f
MgKyaw <mgkyaw20112@gmail.com>
2025-02-05 20:51:49 +0700

    feat: Concate with string interpolation

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -29,4 +29,6 @@ decimal salePrice = 59.99m;
 
 string yourDiscount = string.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
 
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!";
+
 Console.WriteLine(yourDiscount);
