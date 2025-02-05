29794a8e929657dfb2ca18d17a944d8f0eba840c
MgKyaw <mgkyaw20112@gmail.com>
2025-02-05 20:56:31 +0700

    feat: Intialize variables and display

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -32,3 +32,13 @@
 // yourDiscount += $"A discount of {((price - salePrice)/price):P2}!";
 
 // Console.WriteLine(yourDiscount);
+
+// Unit 3
+
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
