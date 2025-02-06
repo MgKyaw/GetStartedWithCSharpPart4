ccca09cb2e60d3979b93028876fb877f61604128
MgKyaw <mgkyaw20112@gmail.com>
2025-02-06 22:17:50 +0700

    feat: Comment PadLeft and PadRight testing

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -49,10 +49,10 @@
 
 // Unit 4
 
-string input = "Pad this";
-Console.WriteLine(input.PadLeft(12));
-Console.WriteLine($"{input.PadRight(12)}.");
+// string input = "Pad this";
+// Console.WriteLine(input.PadLeft(12));
+// Console.WriteLine($"{input.PadRight(12)}.");
 
-Console.WriteLine(input.PadLeft(12, '-'));
-Console.WriteLine(input.PadRight(12, '-'));
+// Console.WriteLine(input.PadLeft(12, '-'));
+// Console.WriteLine(input.PadRight(12, '-'));
 
