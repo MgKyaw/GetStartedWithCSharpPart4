148e15c3f07567f99d1af5bd8718bc1caab7c1b1
MgKyaw <mgkyaw20112@gmail.com>
2025-02-06 22:23:44 +0700

    feat: Add the payment amount to the output

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -58,9 +58,11 @@
 
 string paymentId = "769C";
 string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
 
 var formattedLine = paymentId.PadRight(6);
 formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
 
 Console.WriteLine(formattedLine);
 
