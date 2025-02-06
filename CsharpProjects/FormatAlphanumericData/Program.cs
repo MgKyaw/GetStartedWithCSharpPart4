de95d3a424239dfa749d8cdd59fd2073a0e2fae9
MgKyaw <mgkyaw20112@gmail.com>
2025-02-06 22:21:28 +0700

    feat: Concat and display payeeName

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -57,8 +57,10 @@
 // Console.WriteLine(input.PadRight(12, '-'));
 
 string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
 
 var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
 
 Console.WriteLine(formattedLine);
 
