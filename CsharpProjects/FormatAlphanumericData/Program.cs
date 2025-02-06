15df3ec9c002758d421bdbb1c42426d36d286d37
MgKyaw <mgkyaw20112@gmail.com>
2025-02-06 22:27:49 +0700

    feat: Comment Unit 4

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -47,23 +47,23 @@
 // Console.WriteLine($"            Tax: {taxPercentage:P2}");
 // Console.WriteLine($"        Total Billed: {total:C}");
 
-// Unit 4
+// // Unit 4
 
-// string input = "Pad this";
-// Console.WriteLine(input.PadLeft(12));
-// Console.WriteLine($"{input.PadRight(12)}.");
+// // string input = "Pad this";
+// // Console.WriteLine(input.PadLeft(12));
+// // Console.WriteLine($"{input.PadRight(12)}.");
 
-// Console.WriteLine(input.PadLeft(12, '-'));
-// Console.WriteLine(input.PadRight(12, '-'));
+// // Console.WriteLine(input.PadLeft(12, '-'));
+// // Console.WriteLine(input.PadRight(12, '-'));
 
-string paymentId = "769C";
-string payeeName = "Mr. Stephen Ortega";
-string paymentAmount = "$5,000.00";
+// string paymentId = "769C";
+// string payeeName = "Mr. Stephen Ortega";
+// string paymentAmount = "$5,000.00";
 
-var formattedLine = paymentId.PadRight(6);
-formattedLine += payeeName.PadRight(24);
-formattedLine += paymentAmount.PadLeft(10);
+// var formattedLine = paymentId.PadRight(6);
+// formattedLine += payeeName.PadRight(24);
+// formattedLine += paymentAmount.PadLeft(10);
 
-Console.WriteLine("1234567890123456789012345678901234567890");
-Console.WriteLine(formattedLine);
+// Console.WriteLine("1234567890123456789012345678901234567890");
+// Console.WriteLine(formattedLine);
 
