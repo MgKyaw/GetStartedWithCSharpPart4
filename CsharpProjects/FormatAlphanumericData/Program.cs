7cc3a1b970bdf59db4c7cca7e93fc7a4b7c0b845
MgKyaw <mgkyaw20112@gmail.com>
2025-02-06 22:17:01 +0700

    feat: Use PadLeft and PadRight with custom character

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -53,3 +53,6 @@ string input = "Pad this";
 Console.WriteLine(input.PadLeft(12));
 Console.WriteLine($"{input.PadRight(12)}.");
 
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+
