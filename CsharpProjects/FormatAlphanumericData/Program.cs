090006a925ac1f7096d2bbfaa645bc2f3edd7eab
MgKyaw <mgkyaw20112@gmail.com>
2025-02-07 22:44:47 +0700

    feat: Format the desired output

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -80,12 +80,18 @@ string newProduct = "Glorious Future";
 decimal newReturn = 0.13125m;
 decimal newProfit = 63000000.0m;
 
-// Your logic here
-
+Console.WriteLine($"Dear {customerName}");
+Console.WriteLine("As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+Console.WriteLine("");
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.");
+Console.WriteLine("");
+Console.WriteLine($"Our new product, Glorious Future offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C2}.");
+Console.WriteLine("");
 Console.WriteLine("Here's a quick comparison:\n");
 
 string comparisonMessage = "";
 
-// Your logic here
+comparisonMessage += $"{currentProduct.PadRight(20)}{currentReturn:P2}  {currentProfit:C2}\n";
+comparisonMessage += $"{newProduct.PadRight(20)}{newReturn:P2}  {newProfit:C2}\n";
 
 Console.WriteLine(comparisonMessage);
