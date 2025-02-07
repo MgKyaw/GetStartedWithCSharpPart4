a541d823e60a3f2cf5706798c177335af72bd9a6
MgKyaw <mgkyaw20112@gmail.com>
2025-02-07 22:03:42 +0700

    feat: Initialize variables and display a message

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -67,3 +67,25 @@
 // Console.WriteLine("1234567890123456789012345678901234567890");
 // Console.WriteLine(formattedLine);
 
+// Unit 5
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
