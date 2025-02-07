9f338ff49854aba98b2e69e58099092432d91ab2
MgKyaw <mgkyaw20112@gmail.com>
2025-02-07 22:47:10 +0700

    feat: Add the code from the module

--- CsharpProjects/FormatAlphanumericData/Program.cs
+++ CsharpProjects/FormatAlphanumericData/Program.cs
@@ -94,4 +94,22 @@ string comparisonMessage = "";
 // comparisonMessage += $"{currentProduct.PadRight(20)}{currentReturn:P2}  {currentProfit:C2}\n";
 // comparisonMessage += $"{newProduct.PadRight(20)}{newReturn:P2}  {newProfit:C2}\n";
 
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
 Console.WriteLine(comparisonMessage);
