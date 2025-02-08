3575fd34bec5377dece9fd4910485a9a1d42618f
MgKyaw <mgkyaw20112@gmail.com>
2025-02-08 21:16:24 +0700

    feat: Display the value between span tag

--- CsharpProjects/ModifyContent/Program.cs
+++ CsharpProjects/ModifyContent/Program.cs
@@ -10,3 +10,13 @@
 
 // int length = closingPosition - openingPosition;
 // Console.WriteLine(message.Substring(openingPosition, length));
+
+string message = "What is the value <span>between the tags</span>?";
+
+int openingPosition = message.IndexOf("<span>");
+int closingPosition = message.IndexOf("</span>");
+
+openingPosition += 6;
+
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
