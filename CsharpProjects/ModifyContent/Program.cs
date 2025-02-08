cdd46026f6fa82a11876133e82cb2c529522669f
MgKyaw <mgkyaw20112@gmail.com>
2025-02-08 21:08:42 +0700

    feat: Find and display opening and closing position

--- CsharpProjects/ModifyContent/Program.cs
+++ CsharpProjects/ModifyContent/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
