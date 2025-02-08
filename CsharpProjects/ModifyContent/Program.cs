b7e3c7f13eb48bb61ca5152998ab09940657a92e
MgKyaw <mgkyaw20112@gmail.com>
2025-02-08 21:23:50 +0700

    feat: Comment IndexOf and LastIndexOf method

--- CsharpProjects/ModifyContent/Program.cs
+++ CsharpProjects/ModifyContent/Program.cs
@@ -26,9 +26,9 @@
 
 // Unit 3
 
-string message = "hello there!";
+// string message = "hello there!";
 
-int first_h = message.IndexOf('h');
-int last_h = message.LastIndexOf('h');
+// int first_h = message.IndexOf('h');
+// int last_h = message.LastIndexOf('h');
 
-Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
+// Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
