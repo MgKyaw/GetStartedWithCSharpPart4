da5a55e9f2ac7430abc4f1c4a8055059851be638
MgKyaw <mgkyaw20112@gmail.com>
2025-02-09 10:00:51 +0700

    feat: Comment using Substring with startPosition

--- CsharpProjects/ModifyContent/Program.cs
+++ CsharpProjects/ModifyContent/Program.cs
@@ -63,13 +63,13 @@
 
 // Console.WriteLine($"Found '{message[index]}' in '{message}' at index: {index}.");
 
-string message = "Help (find) the {opening symbols}";
-Console.WriteLine($"Searching THIS Message: {message}");
+// string message = "Help (find) the {opening symbols}";
+// Console.WriteLine($"Searching THIS Message: {message}");
 
-char[] openSymbols = { '[', '{', '(' };
-int startPosition = 5;
-int openingPosition = message.IndexOfAny(openSymbols);
-Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
+// char[] openSymbols = { '[', '{', '(' };
+// int startPosition = 5;
+// int openingPosition = message.IndexOfAny(openSymbols);
+// Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
 
-openingPosition = message.IndexOfAny(openSymbols, startPosition);
-Console.WriteLine($"Found WITH using startPosition {startPosition}:  {message.Substring(openingPosition)}");
+// openingPosition = message.IndexOfAny(openSymbols, startPosition);
+// Console.WriteLine($"Found WITH using startPosition {startPosition}:  {message.Substring(openingPosition)}");
