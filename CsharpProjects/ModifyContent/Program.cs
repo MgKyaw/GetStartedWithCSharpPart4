0a525452de2a196e3948fa5de1e94df09402735f
MgKyaw <mgkyaw20112@gmail.com>
2025-02-09 09:53:10 +0700

    feat: Use Substring method with and without startPosition

--- CsharpProjects/ModifyContent/Program.cs
+++ CsharpProjects/ModifyContent/Program.cs
@@ -62,3 +62,14 @@
 // int index = message.IndexOfAny(charsToFind);
 
 // Console.WriteLine($"Found '{message[index]}' in '{message}' at index: {index}.");
+
+string message = "Help (find) the {opening symbols}";
+Console.WriteLine($"Searching THIS Message: {message}");
+
+char[] openSymbols = { '[', '{', '(' };
+int startPosition = 5;
+int openingPosition = message.IndexOfAny(openSymbols);
+Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
+
+openingPosition = message.IndexOfAny(openSymbols, startPosition);
+Console.WriteLine($"Found WITH using startPosition {startPosition}:  {message.Substring(openingPosition)}");
