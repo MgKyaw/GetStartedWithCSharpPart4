0e26bcb48d330d3ae173334a10c8157fe3a2eb67
MgKyaw <mgkyaw20112@gmail.com>
2025-02-09 11:19:58 +0700

    feat: Comment Replace method

--- CsharpProjects/ModifyContent/Program.cs
+++ CsharpProjects/ModifyContent/Program.cs
@@ -131,7 +131,7 @@
 // string updatedData = data.Remove(5, 20);
 // Console.WriteLine(updatedData);
 
-string message = "This--is--ex-amp-le--da-ta";
-message = message.Replace("--", " ");
-message = message.Replace("-", "");
-Console.WriteLine(message);
+// string message = "This--is--ex-amp-le--da-ta";
+// message = message.Replace("--", " ");
+// message = message.Replace("-", "");
+// Console.WriteLine(message);
