830b9e79f01211c8d83012da148887d7495a07af
MgKyaw <mgkyaw20112@gmail.com>
2025-02-09 09:30:05 +0700

    feat: Retrieve all substrings via while loop

--- CsharpProjects/ModifyContent/Program.cs
+++ CsharpProjects/ModifyContent/Program.cs
@@ -40,3 +40,18 @@
 // int closingPosition = message.LastIndexOf(')');
 // int length = closingPosition - openingPosition;
 // Console.WriteLine(message.Substring(openingPosition, length));
+
+string message = "(What if) there are (more than) one (set of parentheses)?";
+
+while(true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
+
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+
+    message = message.Substring(closingPosition + 1);
+}
