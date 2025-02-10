05579edacf638608e7989689b9ac92004ed31f55
MgKyaw <mgkyaw20112@gmail.com>
2025-02-10 20:59:19 +0700

    feat: create input and display the output

--- CsharpProjects/ModifyContent/Program.cs
+++ CsharpProjects/ModifyContent/Program.cs
@@ -135,3 +135,15 @@
 // message = message.Replace("--", " ");
 // message = message.Replace("-", "");
 // Console.WriteLine(message);
+
+// Unit 5
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
